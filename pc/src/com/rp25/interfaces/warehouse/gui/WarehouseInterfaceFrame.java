07674062fef92283443a91b3176e3d3e41de3c33
ass782 <ass782@student.bham.ac.uk>
2018-03-07 23:38:31 +0000

    Preliminary GUI design implementation

--- pc/src/com/rp25/interfaces/warehouse/gui/WarehouseInterfaceFrame.java
+++ pc/src/com/rp25/interfaces/warehouse/gui/WarehouseInterfaceFrame.java
@@ -5,9 +5,13 @@ import java.awt.GridLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import rp.robotics.mapping.GridMap;
+import rp.robotics.mapping.MapUtils;
+import rp.robotics.visualisation.GridMapVisualisation;
+
 public class WarehouseInterfaceFrame extends JFrame {
 	
-	JPanel visualPanel;
+	GridMapVisualisation visualPanel;
 	JPanel infoPanel;
 
 	public WarehouseInterfaceFrame(String title) {
@@ -16,14 +20,20 @@ public class WarehouseInterfaceFrame extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLayout(new GridLayout(1, 2));
 		
-		visualPanel = new JPanel();
+		GridMap map = MapUtils.createRealWarehouse();
+		visualPanel = new GridMapVisualisation(map, map, 150f); 
+		add(visualPanel);
+		
 		infoPanel = new JPanel();
+		add(infoPanel);
+		
+		setSize(1290, 500);
 		
 		setVisible(true);
 	}
 	
 	public static void main(String[] args) {
-		new WarehouseInterfaceFrame("ASd");
+		new WarehouseInterfaceFrame("Robot Warehouse Interface");
 	}
 	
 }
