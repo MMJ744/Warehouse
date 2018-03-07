4be1af10b23b47a4a4a83714c8c41a4fd042ec2f
ass782 <ass782@student.bham.ac.uk>
2018-03-07 22:02:26 +0000

    Added main method for CLI

---
+++ pc/src/com/rp25/interfaces/warehouse/WarehouseInterfaceMain.java
@@ -0,0 +1,34 @@
+package com.rp25.interfaces.warehouse;
+
+import java.util.ArrayList;
+
+import tools.Job;
+import tools.Robot;
+
+public class WarehouseInterfaceMain {
+	
+	public static void main(String[] args) {
+
+		Job j1 = new Job("job 1");
+		Job j2 = new Job("job 2");
+		Job j3 = new Job("job 3");
+		
+		Robot r1 = new Robot(j1, 1, 3, 0);
+		Robot r2 = new Robot(j2, 2, 2, 4);
+		Robot r3 = new Robot(j3, 3, 3, 6);
+		
+		
+		ArrayList<Robot> rs = new ArrayList<>();
+		rs.add(r1);
+		rs.add(r2);
+		rs.add(r3);
+		
+		WarehouseCLI cli = new WarehouseCLI(rs);
+		String i;
+		
+		while(!(i = cli.getInput()).equalsIgnoreCase("quit")) {
+			System.out.println(cli.getState().getRobot(Integer.parseInt(i)));
+		}
+	}
+
+}
