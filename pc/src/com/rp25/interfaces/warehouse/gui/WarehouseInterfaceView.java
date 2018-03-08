096df84bdfc3e64f739f13559417aee2c40dc628
ass782 <ass782@student.bham.ac.uk>
2018-03-08 14:28:00 +0000

    Implemented a class to handle GUI Components

---
+++ pc/src/com/rp25/interfaces/warehouse/gui/WarehouseInterfaceView.java
@@ -0,0 +1,25 @@
+package com.rp25.interfaces.warehouse.gui;
+
+import java.util.ArrayList;
+
+import com.rp25.interfaces.warehouse.WarehouseState;
+import com.rp25.tools.Robot;
+
+public class WarehouseInterfaceView {
+	WarehouseInterfaceFrame frame;
+	WarehouseState state;
+	
+	public WarehouseInterfaceView(ArrayList<Robot> list) {
+		frame = new WarehouseInterfaceFrame("Robot Warehouse Interface");
+		state = new WarehouseState();
+		initialise(list);
+		frame.setVisible(true);
+	}
+	
+	private void initialise(ArrayList<Robot> list) {
+		for (Robot r : list) {
+			state.addRobot(r.getID(), r);
+			frame.addInfo(r.toString());
+		}
+	}
+}
