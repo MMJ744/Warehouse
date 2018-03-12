603fa2f31ddfef615b99a3c108392daf9e682dd0
Matthew Jones <mmj744@cs.bham.ac.uk>
2018-03-12 13:20:40 +0000

    fixed tests

--- pc/src/tests/RoutePlanningTest.java
+++ pc/src/tests/RoutePlanningTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.Queue;
+
 import org.apache.log4j.Logger;
 import org.junit.*;
 
@@ -16,7 +18,7 @@ public class RoutePlanningTest {
 	@Test
 	public void pathShouldBeBuilt(Point start, Point goal) {
 		AStarSearch tester = new AStarSearch(start, goal);
-		ArrayList<Point> path = new ArrayList<Point>();
+		Queue<Point> path = new Queue<Point>();
 		
 		path = tester.search();
 		
@@ -26,14 +28,14 @@ public class RoutePlanningTest {
 	@Test
 	public void searchShouldReachGoal(Point start, Point goal,  ArrayList<Point> expectedPath) {
 		AStarSearch tester = new AStarSearch(start, goal);
-		ArrayList<Point> path = new ArrayList<Point>();
+		Queue<Point> path = new Queue<Point>();
 		
 		path = tester.search();
 		
 		try {
 			assertNotNull(path);
 			for(int i = 0; i < path.size(); i++) {
-				assertEquals("Incorrect Node in path", expectedPath.get(i), path.get(i));
+				assertEquals("Incorrect Node in path", expectedPath.get(i), path.pop());
 			}
 		}catch(AssertionError e){
 			logger.debug("Search Failed, path most likely null");
