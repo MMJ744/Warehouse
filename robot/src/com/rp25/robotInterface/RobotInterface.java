3316549efa5be2cba14c899e2b6ba51c728bcc23
dgr790 <dgr790@cs.bham.ac.uk>
2018-03-22 16:38:04 +0000

    added receiving info

--- robot/src/com/rp25/robotInterface/RobotInterface.java
+++ robot/src/com/rp25/robotInterface/RobotInterface.java
@@ -10,7 +10,7 @@ import com.rp25.tools.BlockingQueue;
 
 import java.io.*;
 
-class RobotInterface extends Thread{
+class RobotInterface extends Thread {
 
 	String itemCode;
 	int noOfItems;
@@ -19,12 +19,13 @@ class RobotInterface extends Thread{
 	int itemsCollected;
 	boolean itemsDelivered;
 	String function;
-	BlockingQueue<String> queue = new BlockingQueue<String>;
-	
+	java.util.concurrent.BlockingQueue<String> queue;
+
 	public RobotInterface(BlockingQueue<String> jobQueue) { // id of robot (1/2/3)
 		jobComplete = false;
 		itemsCollected = 0;
 		itemsDelivered = false;
+		queue = jobQueue;
 		getInfo();
 		perform();
 	}
@@ -32,39 +33,52 @@ class RobotInterface extends Thread{
 	public void getInfo() {
 		// BufferedReader fromBT = new BufferedReader(new InputStreamReader(System.in));
 		// Will need to receive: itemCode, noOfItems, function
-		// (cancelled/finished/pickup)
-		String info;
+		// (cancelled/finished/pickup) If pickup, send pickup, itemCode & no of items.
+
 	}
 
 	@Override
 	public void run() {
 		while (true) {
+			function = queue.take();
+			if function.equals("pickup") {
+				itemCode = queue.take();
+				String noOfItemsString = queue.take();
+				noOfItems.valueOf(noOfItemsString);
+				pickingUp();
+			}
 			
+				if (function.equals("finished")) {
+					System.out.println("Job " + itemCode + " has been completed");
+					itemsDelivered = true;
+				} else if (function.equals("cancelled")) {
+					System.out.println("Job has been cancelled, dropping items");
+					break;
+				}
+			// send back 1 for true.
 		}
 	}
-	
-	
+
 	private void perform() {
 
 		if (function.equals("pickup")) {
 			pickingUp();
 		}
-		
+
 		while (!itemsDelivered) {
-		if (function.equals("finished")) {
-			System.out.println("Job " + itemCode + " has been completed");
-			itemsDelivered = true;
-		}
-		else if (function.equals("cancelled")) {
-			System.out.println("Job " + itemCode + "has been cancelled");
-			break;
-		}
+			if (function.equals("finished")) {
+				System.out.println("Job " + itemCode + " has been completed");
+				itemsDelivered = true;
+			} else if (function.equals("cancelled")) {
+				System.out.println("Job " + itemCode + "has been cancelled");
+				break;
+			}
 		}
 	}
-	
+
 	/**
-	 * Checks whether the button has been pressed the correct number of times, to pick up the
-	 * correct number of items.
+	 * Checks whether the button has been pressed the correct number of times, to
+	 * pick up the correct number of items.
 	 */
 	private void pickingUp() {
 		itemsCollected = 0;
@@ -75,7 +89,6 @@ class RobotInterface extends Thread{
 				public void buttonReleased(Button _b) {
 					Sound.beep();
 					buttonPressed++;
-					System.out.println(buttonPressed + " items collected.");
 				}
 
 				@Override
@@ -86,19 +99,16 @@ class RobotInterface extends Thread{
 
 			if (buttonPressed == noOfItems) {
 				Sound.twoBeeps();
-				System.out.println("All items for job " + itemCode + " have been collected");
+				System.out.println("All items have been collected");
 				buttonPressed = 0;
 				itemsCollected = 1;
-				System.out.println("Delivering items for job: " + itemCode);
 				sendData();
 			} else if (function.equals("cancelled")) {
-				System.out.println("Job " + itemCode + " has been cancelled");
+				System.out.println("Job has been cancelled");
 				break;
 			}
 		}
-		getInfo();
 	}
-
 
 	public void sendData() {
 		// Need to show when the robot has collected up the correct number of items.
@@ -108,7 +118,8 @@ class RobotInterface extends Thread{
 	 * Returns whether the button on the robot has been pressed the correct no. of
 	 * times
 	 * 
-	 * @return integer showing whether correct no. of items has been collected (0 or 1)
+	 * @return integer showing whether correct no. of items has been collected (0 or
+	 *         1)
 	 */
 	public int getCollected() {
 		return itemsCollected;
